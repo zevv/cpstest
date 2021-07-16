8a3a82884acc47dc02e19197ca5b0270386d74b5
Ico Doornekamp <ico@zevv.nl>
2021-07-16 15:11:27 +0200

    matrix 1

---
+++ matrix.nim
@@ -0,0 +1,48 @@
+
+# Main program
+
+from os import nil
+import json
+import cps
+import types, evq, http, httpserver, httpclient
+
+type
+
+  MatrixClient = ref object
+    url: string
+    token: string
+
+  MatrixLoginReq = object
+    `type`: string
+    user: string
+    password: string
+   
+
+proc newMatrixClient(server: string): MatrixClient =
+  let mc =MatrixClient(
+    url: "http://" & server & "/_matrix/client/r0/"
+  )
+  echo mc.url
+  mc
+
+proc doRequest(mc: MatrixClient, meth: string, req: JsonNode) {.cps:C.} =
+  let client = httpClient.newClient()
+  let rsp = client.request("POST", mc.url & meth, $req)
+  echo rsp
+  echo rsp.body
+
+proc login(mc: MatrixClient, user, password: string) {.cps:C.} =
+  let req = % MatrixLoginReq(
+    type: "m.login.password",
+    user: user,
+    password: password
+  )
+
+  mc.doRequest("login", req)
+
+proc matrix() {.cps:C.} =
+
+  let mc = newMatrixClient("tchncs.de")
+  mc.login("zevv", "123qwe!@#QWE")
+
+
